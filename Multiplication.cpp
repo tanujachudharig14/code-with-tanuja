9ea9fe5f04e96ea363d2069de7e11ded87589739
tanujachudharig14 <tanujachaudhari757@gmail.com>
2026-03-07 12:45:41 +0530

    Add files via upload

---
+++ Multiplication.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2, result;
+    
+    printf("enter first number");
+    scanf("%d", &num1);
+    
+    printf("enter second number");
+    scanf("%d", &num2);
+    
+    result = num1 * num2;
+    
+    printf("multiplication = %d", result);
+    
+    return 0;
+}
